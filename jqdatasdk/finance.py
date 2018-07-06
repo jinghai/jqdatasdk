f0e46bb053834f037877985dc300cfced0cc69a8
wangchaoyang <wangchaoyang@joinquant.com>
2018-07-06 16:03:58 +0800

    jqdatasdk上线新数据无需更新代码

--- jqdatasdk/finance.py
+++ jqdatasdk/finance.py
@@ -10,6 +10,8 @@ import sys
 class Finance(object):
     RESULT_ROWS_LIMIT = 3000
 
+    db_name = "finance"
+
     def __init__(self, disable_join=False):
         self.__disable_join = True
         self.__table_names = self.__load_table_names()
@@ -17,14 +19,19 @@ class Finance(object):
             setattr(self, name, self.__load_table_class(name))
 
     def __load_table_names(self):
-        import os
-        tables_dir = os.path.join(sys.modules["ROOT_DIR"], 'fin_tables')
-        names = []
-        if os.path.exists(tables_dir):
-            for table_file in os.listdir(tables_dir):
-                if table_file.endswith('.py') and not table_file.startswith('__'):
-                    names.append(table_file[:-3])
-        return names
+        # import os
+        # tables_dir = os.path.join(sys.modules["ROOT_DIR"], 'fin_tables')
+        # names = []
+        # if os.path.exists(tables_dir):
+        #     for table_file in os.listdir(tables_dir):
+        #         if table_file.endswith('.py') and not table_file.startswith('__'):
+        #             names.append(table_file[:-3])
+        # return names
+
+        lst = JQDataClient.instance().get_table_orm(self.db_name)
+        ####
+
+        return lst
 
     @assert_auth
     def run_query(self, query_object):
@@ -42,8 +49,35 @@ class Finance(object):
         return df
 
     def __load_table_class(self, table_name):
-        table_module = __import__('jqdatasdk.fin_tables.' + table_name, fromlist=[table_name])
-        return getattr(table_module, table_name)
+        """
+        服务端返回结果示例：
+
+        data = {"name": "IDX_WEIGHT_MONTH", 
+                "columns": 
+                    [
+                        ("id", 'Column(Integer, primary_key=True)'), 
+                        ("index_code", 'Column(String(12), nullable=False, comment="指数代码")'), 
+                        ("end_date", 'Column(Date, nullable=False, comment="截止日期")'), 
+                        ("weight", 'Column(DECIMAL(10, 4), nullable=False, comment="权重")'), 
+                        ("status", 'Column(TINYINT(display_width=4), default=0, comment="是否同步,0:未同步,1:已同步")'), 
+                        ("addTime", 'Column(TIMESTAMP, default=datetime.datetime.now, comment="插入时间")'), 
+                        ("modTime", 'Column(TIMESTAMP, default=datetime.datetime.now, comment="最后修改时间")'), 
+                    ]}
+        """
+        import datetime
+        from sqlalchemy import Date, Column, DateTime, Integer, Numeric, SmallInteger, String, Table, Text, text
+        from sqlalchemy.dialects.mysql import TINYINT, TIMESTAMP, DECIMAL
+        from sqlalchemy.ext.declarative import declarative_base
+
+        data = JQDataClient.instance().get_table_orm(self.db_name, table_name)
+
+        dct = {}
+        for k, v in data["columns"]:
+            column = eval(v)
+            dct[k] = column
+            column.name = k
+        dct["__tablename__"] = data["name"]
+        return type(data["name"], (declarative_base(),), dct)
 
     # def __getattribute__(self, key):
     #     v = object.__getattribute__(self, key)
